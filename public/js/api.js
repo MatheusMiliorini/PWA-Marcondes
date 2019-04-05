f8a219ad08094ed329e5077e3bbefdefd43f6fa2
Matheus Miliorini <miliorinimatheus@gmail.com>
2019-04-04 21:22:29 -0300

    CRUD Produtos OK

--- public/js/api.js
+++ public/js/api.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
     // Busca os produtos e preenche a tela
     $.ajax({
-        url: "http://localhost/aula_21_03/API.php",
+        url: "API.php",
         method: "POST",
         data: {
             op: "TP"
         },
         success: function(data) {
             data.forEach(produto => {
-                html  = `<div class="col-3"><h4>${produto.nome}</h4>`;
+                html  = `<div class="col-12 col-md-3"><h4>${produto.nome}</h4>`;
                 html += `<h6>${produto.descricao}</h6>`;
                 html += `<h6>R$ ${produto.valor}</h6>`;
                 html += `<h6>Categoria: ${produto.nome_categoria}</h6>`;
@@ -25,13 +25,15 @@ $(document).ready(function() {
                 html += `</div>`;
                 $(".listaProdutos").append(html);
             });
+            html = `<div class="col-12 col-md-3" style="display:flex; align-items: center; min-height: 150px"><button class="btn btn-warning adicionar">Adicionar</button></div>`;
+            $(".listaProdutos").append(html);
         }
     });
 
     $(".listaProdutos").on('click', '.excluir', function () {
         if (confirm("Tem certeza que deseja excluir este produto?")) {
             $.ajax({
-                url: "http://localhost/aula_21_03/API.php",
+                url: "API.php",
                 method: "POST",
                 data: {
                     op: "DP",
@@ -43,6 +45,7 @@ $(document).ready(function() {
             });
         }
     });
+
     $(".listaProdutos").on('click', '.editar', function () {
         let div = document.createElement("div");
         let html = `
@@ -65,7 +68,7 @@ $(document).ready(function() {
                 categoria = $("#editCat").val(),
                 produto_id = $("#editProdutoID").val();
             $.ajax({
-                url: "http://localhost/aula_21_03/API.php",
+                url: "API.php",
                 method: "POST",
                 data: {
                     op: "EP",
@@ -91,5 +94,48 @@ $(document).ready(function() {
         });
     });
 
+    $(".listaProdutos").on('click', '.adicionar', function () {
+        let div = document.createElement("div");
+        let html = `
+            <label>Nome:</label><input id="addNome" type="text" class="form-control">
+            <label>Descrição:</label><input id="addDesc" type="text" class="form-control">
+            <label>Valor:</label><input id="addVal" type="number" step=".01" class="form-control">
+            <label>Categoria:</label><input id="addCat" type="text" step=".01" class="form-control">
+        `;
+        div.setAttribute("style","text-align: left");
+        $(div).append(html);
+        swal({
+            title: "Adicionar produto",
+            content: div,
+        }).then(ok => {
+            if (!ok) return false;
+            let nome = $("#addNome").val(),
+                desc = $("#addDesc").val(),
+                valor = $("#addVal").val(),
+                categoria = $("#addCat").val();
+            $.ajax({
+                url: "API.php",
+                method: "POST",
+                data: {
+                    op: "AP",
+                    nome: nome,
+                    descricao: desc,
+                    valor: valor,
+                    categoria: categoria,
+                },
+                success: function(data) {
+                    if (data === 1) {
+                        location.reload();
+                    } else {
+                        swal({
+                            title: "Erro!",
+                            icon: "error",
+                            text: "Verifique se a categoria indicada existe!",
+                        });
+                    }
+                }
+            });
 
+        });
+    });
 });
